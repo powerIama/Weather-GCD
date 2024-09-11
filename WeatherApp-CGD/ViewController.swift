d561f03a10b4f6a8ee31eef34e490c2bf499bf09
Zukk <basicdemich@gmail.com>
2024-09-11 16:01:30 -0500

    Initial Commit

---
+++ WeatherApp-CGD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp-CGD
+//
+//  Created by Django on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
